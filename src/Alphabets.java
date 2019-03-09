945c6b4111e4e6e1f61e1f5c056384c7f744a832
Nirupa03 <nirupa0312@gmail.com>
2019-03-09 18:32:26 +0000

    My commit

---
+++ src/Alphabets.java
@@ -0,0 +1,16 @@
+public class Alphabets {
+
+    //static char a = 'A';
+    //static char b = 'B';
+
+
+    public static void main(String[] args) {
+        //System.out.println(a);
+        //System.out.println(b);
+        //for (int i = 0; i < 26 ; i++) {
+           char ch ;
+           for (ch = 'A'; ch <= 'Z' ; ch++){
+               System.out.println(ch);
+        }
+    }
+}
